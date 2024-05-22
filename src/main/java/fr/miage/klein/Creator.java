1da6aae305bcadf7546fa4b47c534c15f3fd4d75
Guatier Klein <klein_gautier@yahoo.fr>
2024-05-22 15:25:58 +0200

    added create client

--- src/main/java/fr/miage/klein/Creator.java
+++ src/main/java/fr/miage/klein/Creator.java
@@ -1,7 +1,23 @@
 package fr.miage.klein;
 
+import java.util.Scanner;
+
 public class Creator {
-    public static void createClientInteractive() {
+    public static Client createClientInteractive() {
+        Scanner in = new Scanner(System.in);
         System.out.println("Création interactive d'un client :");
+        System.out.print("Nom : ");
+        String nom = in.nextLine();
+        System.out.print("Prénom : ");
+        String prenom = in.nextLine();
+        System.out.print("Adresse : ");
+        String adresse = in.nextLine();
+        System.out.print("Num téléphone : ");
+        String numTel = in.nextLine();
+        System.out.print("EMail : ");
+        String email = in.nextLine();
+        System.out.print("Num CB : ");
+        String numCb = in.nextLine();
+        return new Client(prenom, nom, adresse, numTel, email, numCb); 
     }
 }
