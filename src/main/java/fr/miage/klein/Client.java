a2a8efabc0ae8421944714e62a3df45da1c349d4
OlivierJe <olivierjeandel1@gmail.com>
2024-05-22 15:01:13 +0200

    Create class Client + Mail

--- src/main/java/fr/miage/klein/Client.java
+++ src/main/java/fr/miage/klein/Client.java
@@ -1,5 +1,79 @@
 package fr.miage.klein;
 
 public class Client {
+    private String prenom;
+    private String nom;
+    private String adresse;
+    private String numTel;
+    private Mail email;
+    private long numCb;
+    private String immat;
 
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getAdresse() {
+        return this.adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getNumTel() {
+        return this.numTel;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
+    }
+
+    public Mail getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(Mail email) {
+        this.email = email;
+    }
+
+    public long getNumCb() {
+        return this.numCb;
+    }
+
+    public void setNumCb(long numCb) {
+        this.numCb = numCb;
+    }
+
+    public String getImmat() {
+        return this.immat;
+    }
+
+    public void setImmat(String immat) {
+        this.immat = immat;
+    }
+
+    public Client(String prenom, String nom, String adresse, String numTel, Mail email, long numCb, String immat) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.numTel = numTel;
+        this.email = email;
+        this.numCb = numCb;
+        this.immat = immat;
+    }
+
+    public Client(String prenom, String nom, String adresse, String numTel, Mail email, long numCb) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.numTel = numTel;
+        this.email = email;
+        this.numCb = numCb;
+    }
 }
--- src/main/java/fr/miage/klein/Main.java
+++ src/main/java/fr/miage/klein/Main.java
@@ -3,5 +3,6 @@ package fr.miage.klein;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Client client = new Client("Olivier", "Jeandel", "58 boulevard Pompidou, Saint Dié des Vosges", "0663812317", new Mail("tttttt@tttttt.tttttt"), 0);
     }
 }
