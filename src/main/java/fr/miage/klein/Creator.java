00a9ab5cb877e86b114c5663ca07f9db3937d2ed
Guatier Klein <klein_gautier@yahoo.fr>
2024-05-22 15:04:47 +0200

    added creator class

---
+++ src/main/java/fr/miage/klein/Creator.java
@@ -0,0 +1,7 @@
+package fr.miage.klein;
+
+public class Creator {
+    public static void createClientInteractive() {
+        System.out.println("Création interactive d'un client :");
+    }
+}
--- src/main/java/fr/miage/klein/Interpreter.java
+++ src/main/java/fr/miage/klein/Interpreter.java
@@ -1,7 +1,27 @@
 package fr.miage.klein;
 
 public class Interpreter {
-    public void execute() {
+    public void execute(Command command) {
+        switch (command.getCommand()) {
+            case "create":
+                create(command.getArgs());
+                break;
         
+            default:
+                throw new IllegalArgumentException("Commande inexistante");
+        }
+    }
+
+    private void create(String[] args) {
+        if(args.length == 0)
+            throw new IllegalArgumentException("Arguments invalides");
+        switch (args[0]) {
+            case "client":
+                Creator.createClientInteractive();
+                break;
+        
+            default:
+            throw new IllegalArgumentException("Arguments invalides");
+        }
     }
 }
