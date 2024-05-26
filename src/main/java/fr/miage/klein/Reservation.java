ec0c292ad8a001d35754941e3aad4d43fba00616
OlivierJe <olivierjeandel1@gmail.com>
2024-05-26 14:44:51 +0200

    fear: Reservation class creating

---
+++ src/main/java/fr/miage/klein/Reservation.java
@@ -0,0 +1,28 @@
+package fr.miage.klein;
+
+import java.time.LocalDateTime;
+
+public class Reservation {
+    int id;
+    LocalDateTime datetime;
+    int duree;
+    EResEtat etat;
+    Mail mailClient;
+    String immat;
+    int idBorne;
+
+    public Reservation(int id, LocalDateTime datetime, int duree, EResEtat etat, Mail mailClient, String immat, int idBorne) {
+        this.id = id;
+        this.datetime = datetime;
+        this.duree = duree;
+        this.etat = etat;
+        this.mailClient = mailClient;
+        this.immat = immat;
+        this.idBorne = idBorne;
+    }
+
+    public void changerEtat(EResEtat etat){
+        this.etat = etat;
+    }
+
+}
