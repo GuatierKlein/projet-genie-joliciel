b4d9eaaad01f68c3ae53a5df646bb486e89b13ec
MullerTheodore <theodore.muller31@gmail.com>
2024-05-26 05:02:16 +0200

    feat : add main commands to test factures generation in different scenarios

--- src/main/java/fr/miage/klein/Facture.java
+++ src/main/java/fr/miage/klein/Facture.java
@@ -1,12 +1,13 @@
 package fr.miage.klein;
 
+import java.sql.Time;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 public class Facture {
     
     public static final String deviseParDefaut = "EUR";
     public static final float tarifHoraireRes = 10;
+    public static final float dureeProlongementEnHeure = (float) 0.25; // 15 min
     public static final float tarifSupplementResRetard = 2;
     public static final float tarifHoraireCharge = 5;
     public static final float tarifPenaliteInitial = 1;
@@ -17,31 +18,36 @@ public class Facture {
     private Frais fraisRecharge;
     private Frais fraisReservation;
     private Frais fraisPenalite;
+    private Frais fraisProlongation;
 
-    public Facture(Frais fraisRecharge, Frais fraisReservation, Frais fraisPenalite, LocalDateTime dateFacture) {
+    public Facture(Frais fraisRecharge, Frais fraisReservation, Frais fraisPenalite, Frais fraisProlongation, LocalDateTime dateFacture) {
         this.fraisRecharge = fraisRecharge;
         this.fraisReservation = fraisReservation;
         this.fraisPenalite = fraisPenalite;
         this.dateFacture = dateFacture;
     }
     
-    private float calculFraisReservation(LocalTime dureeCreneau, boolean accepteSupplement){
+    public static float calculFraisReservation(Time dureeCreneau, boolean accepteSupplement){
         if (accepteSupplement) 
-            return  tarifSupplementResRetard + dureeCreneau.getHour() * tarifHoraireRes;
-        return dureeCreneau.getHour() * tarifHoraireRes;
+            return  tarifSupplementResRetard + dureeCreneau.getTime()/3600000 * tarifHoraireRes;
+        return dureeCreneau.getTime()/3600000 * tarifHoraireRes;
         }
 
-    private float calculFraisPenalite(LocalDateTime dureeEnTrop){
-        var coutPen = tarifPenaliteInitial + Math.pow(tauxAugmentationPenParMinute,dureeEnTrop.getMinute());
+    public static float calculFraisProlongementReservation(){ 
+        return  dureeProlongementEnHeure * tarifHoraireRes; 
+        }
+
+    public static float calculFraisPenalite(Time dureeEnTrop){
+        var coutPen = tarifPenaliteInitial + Math.pow(tauxAugmentationPenParMinute,dureeEnTrop.getTime()/60000);
         return (float) coutPen;
     }
 
-    private float calculFraisRecharge(LocalDateTime dureeCharge){
-        return dureeCharge.getHour() * tarifHoraireCharge;
+    public static float calculFraisRecharge(Time dureeCharge){
+        return (float) dureeCharge.getTime()/3600000 * tarifHoraireCharge;
     }
     
-    private float calcuFraisTotaux(float fraisReservation, float fraisRecharge, float fraisPenalite) {
-        return fraisReservation + fraisRecharge + fraisPenalite;
+    public float calcuFraisTotaux(float fraisReservation, float fraisRecharge, float fraisPenalite, float fraisProlongation) {
+        return fraisReservation + fraisRecharge + fraisPenalite + fraisProlongation;
     }
     
     
