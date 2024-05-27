a83e7b57994ffa3eb3b857ea05214a2b009c67a4
OlivierJe <olivierjeandel1@gmail.com>
2024-05-27 15:55:05 +0200

    feat: Conctructor Reservation for new Reservation from code

--- src/main/java/fr/miage/klein/BusinessLogic/Reservation.java
+++ src/main/java/fr/miage/klein/BusinessLogic/Reservation.java
@@ -3,13 +3,13 @@ package fr.miage.klein.BusinessLogic;
 import java.time.LocalDateTime;
 
 public class Reservation {
-    int id;
-    LocalDateTime datetime;
-    int duree;
-    EResEtat etat;
-    Mail mailClient;
-    String immat;
-    int idBorne;
+    private int id;
+    private LocalDateTime datetime;
+    private int duree;
+    private EResEtat etat;
+    private Mail mailClient;
+    private String immat;
+    private int idBorne;
 
     public Reservation(int id, LocalDateTime datetime, int duree, EResEtat etat, Mail mailClient, String immat, int idBorne) {
         this.id = id;
@@ -21,8 +21,67 @@ public class Reservation {
         this.idBorne = idBorne;
     }
 
-    public void changerEtat(EResEtat etat){
+    public Reservation(LocalDateTime datetime, int duree, Mail mailClient, String immat) {
+        this.datetime = datetime;
+        this.duree = duree;
+        this.mailClient = mailClient;
+        this.immat = immat;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDatetime() {
+        return this.datetime;
+    }
+
+    public void setDatetime(LocalDateTime datetime) {
+        this.datetime = datetime;
+    }
+
+    public int getDuree() {
+        return this.duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public EResEtat getEtat() {
+        return this.etat;
+    }
+
+    public void setEtat(EResEtat etat) {
         this.etat = etat;
+    }
+
+    public Mail getMailClient() {
+        return this.mailClient;
+    }
+
+    public void setMailClient(Mail mailClient) {
+        this.mailClient = mailClient;
+    }
+
+    public String getImmat() {
+        return this.immat;
+    }
+
+    public void setImmat(String immat) {
+        this.immat = immat;
+    }
+
+    public int getIdBorne() {
+        return this.idBorne;
+    }
+
+    public void setIdBorne(int idBorne) {
+        this.idBorne = idBorne;
     }
 
 }
