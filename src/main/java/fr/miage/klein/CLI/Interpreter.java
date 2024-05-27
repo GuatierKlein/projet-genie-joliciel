746a4ca8915349f2578e4008881cfc4d6b54d5c3
Guatier Klein <klein_gautier@yahoo.fr>
2024-05-27 16:35:58 +0200

    refactoring

--- src/main/java/fr/miage/klein/CLI/Interpreter.java
+++ src/main/java/fr/miage/klein/CLI/Interpreter.java
@@ -3,7 +3,9 @@ package fr.miage.klein.CLI;
 import java.text.ParseException;
 
 import fr.miage.klein.BusinessLogic.Client;
-import fr.miage.klein.BusinessLogic.Reservation;
+import fr.miage.klein.BusinessLogic.ParkAccess.ParkAccess;
+import fr.miage.klein.BusinessLogic.ParkAccess.ParkAccessImmat;
+import fr.miage.klein.BusinessLogic.ParkAccess.ParkAccessRes;
 import fr.miage.klein.Controller.IDatabaseController;
 
 public class Interpreter {
@@ -13,11 +15,14 @@ public class Interpreter {
         this.db = db;
     }
 
-    public void execute(Command command) {
+    public void execute(Command command) throws Exception {
         switch (command.getCommand()) {
             case "": break;
             case "create":
                 create(command.getArgs());
+                break;
+            case "access":
+                access(command.getArgs());
                 break;
             case "help":
                 help();
@@ -38,29 +43,42 @@ public class Interpreter {
         System.out.println("**********");
     }
 
-    private void create(String[] args) {
+    private void create(String[] args) throws ParseException {
         if(args.length == 0)
             throw new IllegalArgumentException("Arguments invalides");
 
-        try {
-            switch (args[0]) {
-                case "help":
-                    Creator.helpCreate();
-                    break;
-                case "client":
-                    Client newClient = Creator.createClientInteractive();
-                    db.addClient(newClient);
-                    break;
-                case "reservation":
-                    Reservation reserv = Creator.createReservationIntractive();
-                    db.addReservation(reserv);
-                    break;
-                default:
-                throw new IllegalArgumentException("Arguments invalides");
-            }
-        } catch (Exception e) {
-            System.out.println("Saisie invalide ou erreur");
+        switch (args[0]) {
+            case "help":
+                Creator.helpCreate();
+                break;
+            case "client":
+                Client newClient = Creator.createClientInteractive();
+                db.addClient(newClient);
+                break;
+        
+            default:
+            throw new IllegalArgumentException("Arguments invalides");
         }
+    }
 
+    private void access(String[] args) throws Exception {
+        if(args.length < 2 || args.length > 2)
+            throw new IllegalArgumentException("Arguments invalides");
+
+        ParkAccess access;
+
+        switch (args[0]) {
+            case "immat":
+                access = new ParkAccessImmat(args[1], db);
+                break;
+            case "reservation":
+                access = new ParkAccessRes(args[1], db);
+                break;
+        
+            default:
+            throw new IllegalArgumentException("Arguments invalides");
+        }
+        
+        System.out.println("Cher client, bienvenue!");
     }
 }
